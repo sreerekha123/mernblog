669fe9512f033e513c864d98d8709776db7ac1ac
sreerekha12345 <sreerekhag709@gmail.com>
2025-01-31 21:04:55 +0530

    my first commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const authController = require('./controllers/authController')
@@ -8,8 +8,24 @@ const multer = require('multer')
 const app = express()
 
 // connect db
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URL, () => console.log('MongoDB has been started successfully'))
+// mongoose.set('strictQuery', false);
+// mongoose.connect(process.env.MONGO_URL, () => console.log('MongoDB has been started successfully'))
+const mongoose = require('mongoose');
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect('mongodb+srv://sreerekhag709:vcnL0VHHhpogh7pL@cluster0.6krr1.mongodb.net/?retryWrites=true&w=majority ', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
+
+connectToDatabase();
+
 
 // routes
 app.use('/images', express.static('public/images'))
@@ -39,4 +55,4 @@ app.post('/upload', upload.single("image"), async(req, res) => {
 })
 
 // connect server
-app.listen(process.env.PORT, () => console.log('Server has been started successfully'))
+app.listen(process.env.PORT, () => console.log('Server has been started successfully'))
